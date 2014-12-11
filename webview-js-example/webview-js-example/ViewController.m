7b8396d43351740594e51d7859d085880223a81d
danilNil <danil.nikiphorov@gmail.com>
2014-12-11 11:48:10 +0600

    scale page to fit

--- webview-js-example/webview-js-example/ViewController.m
+++ webview-js-example/webview-js-example/ViewController.m
@@ -19,12 +19,8 @@
     [super viewDidLoad];
     NSURL *url = [[NSURL alloc] initWithString:@"http://chrisrisner.com"];
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    self.webView.scalesPageToFit = YES;
     [self.webView loadRequest:request];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
