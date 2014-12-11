3044f4880646ff30fdaa74870d5ef71cbe608b7d
danilNil <danil.nikiphorov@gmail.com>
2014-12-11 11:57:16 +0600

    Calling javascript from Objective-c

--- webview-js-example/webview-js-example/ViewController.m
+++ webview-js-example/webview-js-example/ViewController.m
@@ -34,14 +34,16 @@
         NSArray *urlParts = [urlString componentsSeparatedByString:@":"];
         //check to see if we just got the scheme
         if ([urlParts count] > 1) {
-            NSArray *parameters = [[urlParts objectAtIndex:1] componentsSeparatedByString:@"&"];
-            NSString *methodName = [parameters objectAtIndex:0];
-            NSString *variableName = [parameters objectAtIndex:1];
-            
-            NSString *message = [NSString stringWithFormat:@"Obj-c from js with methodname=%@ and variablename=%@", methodName, variableName];
-            
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Great" message:message delegate: self cancelButtonTitle: nil otherButtonTitles: @"OK",nil, nil];
-            [alert show];
+            [self.webView stringByEvaluatingJavaScriptFromString:@"alert('Trigger the JS!');"];
+
+//            NSArray *parameters = [[urlParts objectAtIndex:1] componentsSeparatedByString:@"&"];
+//            NSString *methodName = [parameters objectAtIndex:0];
+//            NSString *variableName = [parameters objectAtIndex:1];
+//            
+//            NSString *message = [NSString stringWithFormat:@"Obj-c from js with methodname=%@ and variablename=%@", methodName, variableName];
+//            
+//            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Great" message:message delegate: self cancelButtonTitle: nil otherButtonTitles: @"OK",nil, nil];
+//            [alert show];
         }
     }
     return YES;
