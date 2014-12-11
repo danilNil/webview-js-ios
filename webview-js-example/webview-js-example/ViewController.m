1bfb57cac4b953a0526017721246accb161e8366
danilNil <danil.nikiphorov@gmail.com>
2014-12-11 11:52:19 +0600

    inject java script

--- webview-js-example/webview-js-example/ViewController.m
+++ webview-js-example/webview-js-example/ViewController.m
@@ -19,6 +19,9 @@
     [super viewDidLoad];
     NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"LocalPage" ofType:@"html" inDirectory:nil];
     NSString* htmlString = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
+    //Append javascript
+    NSString *script = @"<script>alert(\"This is an alert!!\");</script>";
+    htmlString = [htmlString stringByAppendingString:script];
     [self.webView loadHTMLString:htmlString baseURL:nil];
 }
 
